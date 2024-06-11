d444cc72c8641997bb341c7c8ad43b67b2b18056
Diego <100723562+jtoledom1@users.noreply.github.com>
2024-06-11 08:39:10 -0600

    🐛 Limpieza de rama

--- -py/Hellogithub.py
+++
@@ -1,4 +0,0 @@
-print("Hello Github!!!")
-print("Commit después de un cambio fuera de mi terminal")
-print("Esto debería subirse por medio del uso de un alias")
-print("v2")
--- -py/hellogit.py
+++
@@ -1,4 +0,0 @@
-print("Hello World")
-print("V3 main")
-print ("V2 Main")
-print ("v4 Main")
